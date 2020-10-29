89f8a16a93392dd5259ef4e18c424ba9ec709676
metherton <martinetherton63@gmail.com>
2020-10-29 21:17:14 +0100

    Method Notations

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -4,24 +4,44 @@ object MethodNotations extends App {
 
   import scala.language.postfixOps
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
 
     def likes(movie: String): Boolean = movie == favoriteMovie
-    def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie, age)
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
     def unary_! : String = s"$name, what the heck ?!"
     def isAlive: Boolean = true
-    def apply(): String = s"Hi my name is ${name} and I like ${favoriteMovie}"
+    def learns(subject: String): String = s"$name learns $subject"
+    def learnScala  = this learns "Scala"
+    def apply(): String = s"Hi my name is $name and I like $favoriteMovie"
+    def apply(times: Int): String = s"$name watched $favoriteMovie $times times"
+
+
+    override def toString: String = s"Name: ${name}, Favorite film: ${favoriteMovie}, Age: ${age}"
   }
 
-  val mary = new Person("Mary", "Inception")
+  val mary = new Person("Mary", "Inception", 25)
+
+  println(+mary)
+
   println(mary.likes("Inception"))
   println(mary likes "Inception") // equivalent
   // infix notation = operator notation
 
-  // "operators" in scala
-  val tom = new Person("Tom", "Fight Club")
-  println(mary hangOutWith tom)
+  println(mary.learns("scala"))
 
+  println(mary learnScala)
+
+  println(mary(5))
+
+  // "operators" in scala
+  val tom = new Person("Tom", "Fight Club", 28)
+  println(mary + tom)
+
+  println((mary + "the bloody one")())
+
+  println(mary.+(tom))
   println(!mary)
 
   println(mary.unary_!)
@@ -37,5 +57,23 @@ object MethodNotations extends App {
   // apply
   println(mary.apply())
   println(mary())
+
+  /*
+      1. Overload the + operator
+        mary + "the rockstar" => new person "Mary (the rockstar)"
+      2. Add an age to the person class
+        Add a unary + operator => new person with the age + 1
+        +mary => mary with age incremented
+
+        ++
+
+      3. Add a learns method in the Person class takes a string parameter.. => "mary learns scala"
+          Add a learnsScala method (no params), calls learns method with "Scala"
+          use it in prefix notation
+
+      4. Overload the apply method..
+        mary.apply(2)..takes a number.. => "Mary watched Inception 2 times"
+
+   */
 
 }
