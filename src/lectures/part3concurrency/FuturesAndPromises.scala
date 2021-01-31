bee9f3e8404ecc35e4a2478602435032a06c5ccd
metherton <martinetherton63@gmail.com>
2021-01-31 21:07:38 +0100

    Blocking with Await.result

--- src/lectures/part3concurrency/FuturesAndPromises.scala
+++ src/lectures/part3concurrency/FuturesAndPromises.scala
@@ -1,7 +1,8 @@
 package lectures.part3concurrency
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Random, Success}
+import scala.concurrent.duration._
 
 // important for futures
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -102,5 +103,40 @@ object FuturesAndPromises extends App {
   }
 
   val fallbackResult = SocialNetwork.fetchProfile("unknown id").fallbackTo(SocialNetwork.fetchProfile("fb.id.0-dummy"))
+
+  // online banking app
+  case class User(name: String)
+  case class Transaction(sender: String, receiver: String, amount: Double, status: String)
+
+  object BankingApp {
+    val name = "Rock the JVM Banking"
+
+    def fetchUser(name: String): Future[User] = Future {
+      // simulate fetching from DB
+      Thread.sleep(500)
+      User(name)
+    }
+
+    def createTransaction(user: User, merchantName: String, amount: Double): Future[Transaction] = Future {
+      // simulate some process
+      Thread.sleep(1000)
+      Transaction(user.name, merchantName, amount, "SUCCESS")
+    }
+
+    def purchase(username: String, item: String, merchantName: String, cost: Double): String = {
+      // fetch user from DB
+      // create a transaction
+      // WAIT for transaction to finish
+
+      val transactionStatusFuture = for {
+        user <- fetchUser(username)
+        transaction <- createTransaction(user, merchantName, cost)
+      } yield transaction.status
+
+      Await.result(transactionStatusFuture, 2.seconds) // implicit conversions -> pimp my library
+    }
+  }
+
+  println(BankingApp.purchase("Daniel", "iphone 12", "rock the jvm store", 3000))
 
 }
