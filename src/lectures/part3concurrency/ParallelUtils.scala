84121a98c12dd8d894d13419c04bcf39a632cdd6
metherton <martinetherton63@gmail.com>
2021-02-02 22:33:40 +0100

    Implicits intro

---
+++ src/lectures/part3concurrency/ParallelUtils.scala
@@ -0,0 +1,22 @@
+package lectures.part3concurrency
+
+
+
+object ParallelUtils extends App {
+
+
+  // 1 parallel collections
+
+//  val parList = List(1,2,3).par
+//
+//  val aParVector = ParVector[Int](1,2,3)
+
+  /*
+      Seq
+      Vectors
+      Array
+      Map - Hash, Trie
+      Set - Hash, Trie
+   */
+
+}
