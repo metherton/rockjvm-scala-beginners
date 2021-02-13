d6a29492b71c59b47b689d86c1b1dec284b28816
metherton <martinetherton63@gmail.com>
2021-02-13 10:47:00 +0100

    Self types

---
+++ src/lectures/part5typesystem/SelfTypes.scala
@@ -0,0 +1,91 @@
+package lectures.part5typesystem
+
+object SelfTypes extends App {
+
+  // requiring a type to be mixed in
+
+  trait Instrumentalist {
+    def play(): Unit
+  }
+
+  trait Singer { self: Instrumentalist => // SELF TYPE whoever implements Singer to implement Instrumentalist
+
+    // rest of the implementation or API
+    def sing(): Unit
+  }
+
+  class LeadSinger extends Singer with Instrumentalist {
+    override def sing(): Unit = ???
+
+    override def play(): Unit = ???
+  }
+
+//  class Vocalist extends Singer {
+//    override def sing(): Unit = ???
+//  }
+
+
+  val jamesHetfield = new Singer with Instrumentalist {
+    override def sing(): Unit = ???
+
+    override def play(): Unit = ???
+  }
+
+  class Guitarist extends Instrumentalist {
+    override def play(): Unit = println("guitar solo")
+  }
+
+  val ericClapton = new Guitarist with Singer {
+    override def sing(): Unit = ???
+  }
+
+  // vs INHERITANCE
+  class A
+  class B extends A // B iS an A
+
+  trait T
+  trait S { self: T => } // S requires a T
+
+  // CAKE pattern => "dependency injection"
+
+  // DI
+  class Component {
+    // API
+  }
+  class ComponentA extends Component
+  class ComponentB extends Component
+  class DependentComponent(val component: Component) // in SPRING framework injects this for you
+
+  // CAKE pattern
+  trait ScalaComponent {
+    // API
+    def action(x: Int): String
+  }
+
+  trait ScalaDependentComponent { self: ScalaComponent =>
+    def dependentAction(x: Int): String = action(x) + "this rocks"
+  }
+
+  trait ScalaApplication { self: ScalaDependentComponent => }
+
+  // layer 1 - small components
+  trait Picture extends ScalaComponent
+  trait Stats extends ScalaComponent
+
+  // layer 2 - compose
+  trait Profile extends ScalaDependentComponent with Picture
+  trait Analytics extends ScalaDependentComponent with Stats
+
+  // layer 3 - app
+  trait AnalyticsApp extends ScalaApplication with Analytics
+
+  // cyclical dependencies
+
+//  class X extends Y
+//  class Y extends X
+
+
+  trait X { self: Y => }
+  trait Y { self: X => }
+
+}
