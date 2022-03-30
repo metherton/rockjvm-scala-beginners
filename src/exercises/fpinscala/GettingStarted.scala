9fec02ca4eca387e60bb8ceb5ee7dbdc3add21ac
metherton <martinetherton63@gmail.com>
2022-03-30 23:50:27 +0200

    curry compose

--- src/exercises/fpinscala/GettingStarted.scala
+++ src/exercises/fpinscala/GettingStarted.scala
@@ -28,4 +28,31 @@ object GettingStarted extends App {
 
   println(isSorted(Array(7, 2, 3, 4, 5), isIntegerGreaterThan))
 
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C =
+    (b: B) => f(a, b)
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    (a: A) => (b: B) => f(a, b)
+
+  def curryMore[A, B, C, D](f:(A, B, C) => D): A => (B => (C => D)) =
+    (a: A) => (b: B) => (c: C) => f(a, b, c)
+
+  val addA = curry((a: Int, b: Int) => a + b)
+  val addB = addA(1)
+  val res = addB(5)
+  println(res)
+
+  val timesTwo: Int => Int = (a: Int) => a * 2
+  val timesThree: Int => Int = (a: Int) => a * 3
+  val timesFour: Int => Int = (a: Int) => a * 4
+
+  val times6 = timesTwo.andThen(times6)
+
+
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    (a: A) => f(g(a))
+
 }
