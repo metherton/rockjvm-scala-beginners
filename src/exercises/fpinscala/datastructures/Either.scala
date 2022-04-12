488b60e8b53008fb0aa33920bdb7976e4d35ec74
metherton <martinetherton63@gmail.com>
2022-04-13 00:39:11 +0200

    traverse

--- src/exercises/fpinscala/datastructures/Either.scala
+++ src/exercises/fpinscala/datastructures/Either.scala
@@ -14,12 +14,39 @@ trait Either[+E, +A] {
     case Right(a) => Right(a)
 
   }
+
+
+  def sequence[E,A](es: List[Either[E, A]]): Either[E, List[A]] = es match {
+    case Cons(h, t) => h.flatMap( hh => sequence(t).map(x => Cons(hh, x)))
+  }
+
+//  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+//    as match
+//  case Nil => Right(Nil)
+//  case h :: t => (f(h).map2(traverse(t)(f)))(_ :: _)
+
+//  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+//    as match
+//  case Nil => Right(Nil)
+//  case h :: t => (f(h).map2(traverse(t)(f)))(_ :: _)
+//def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+//  for
+//    a <- this
+//  b1 <- b
+//  yield f(a,b1)
+
   def map2[EE >: E,B,C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
     for {
       a <- this
       bb <- b
-    } yield f(a, b)
+    } yield f(a, bb)
   }
+
+    def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+      case Nil => Right(Nil)
+      case Cons(h, t) => (f(h).map2(traverse(t)(f)))(Cons(_, _))
+    }
+
 }
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
@@ -28,6 +55,26 @@ case class Right[+A](value: A) extends Either[Nothing, A]
 object EitherRunner extends App {
 
 
+  def Try[A](a: => A): Either[Exception, A] =
+    try Right(a)
+    catch {case e: Exception => Left(e)}
 
+
+  def insuranceRateQuote(age: Int, numberOfSpeedingTickets: Int): Double = 13
+
+  def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Either[Exception, Double] = {
+   for {
+     a <- Try{age.toInt}
+     tickets <- Try{numberOfSpeedingTickets.toInt}
+   } yield insuranceRateQuote(a, tickets)
+  }
+
+  println(s"parseInsurnace result is" + parseInsuranceRateQuote("23","33"))
+  println(s"parseInsurnace result is" + parseInsuranceRateQuote("hello","33"))
+
+
+  val first = Right(1)
+
+  println(first.traverse(Cons(Right(2), Cons(Right(3), Cons(Right(4), Nil))))(a => Right(a)))
 
 }
--- src/exercises/fpinscala/datastructures/Option.scala
+++ src/exercises/fpinscala/datastructures/Option.scala
@@ -4,35 +4,20 @@ import exercises.fpinscala.datastructures.OptionRunner.{Try}
 
 sealed trait Option[+A] {
 
-  def map[B](f: A => B): Option[B]
-  def flatMap[B](f: A => Option[B]): Option[B]
-  def getOrElse[ B >: A](default: => B): B
-  def orElse[B >: A](ob: => Option[B]): Option[B]
-  def filter(f: A => Boolean): Option[A]
-  def variance(xs: Seq[Double]): Option[Double]
-  def lift[A,B](f: A => B): Option[A] => Option[B]
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C]
-  def sequence[A](a: List[Option[A]]): Option[List[A]]
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]]
-}
-
-
-
-case class Some[+A](get: A) extends Option[A] {
-  override def flatMap[B](f: A => Option[B]): Option[B] = {
+  def flatMap[B](f: A => Option[B]): Option[B] = {
     map(f).getOrElse(None)
   }
 
-  override def getOrElse[B >: A](default: => B): B = this match {
+  def getOrElse[B >: A](default: => B): B = this match {
     case Some(a) => a
     case _ => default
   }
 
 
-  override def orElse[B >: A](ob: => Option[B]): Option[B] =
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
     map(v => Some(v)).getOrElse(ob)
 
-  override def filter(f: A => Boolean): Option[A] = this match {
+  def filter(f: A => Boolean): Option[A] = this match {
     case Some(a) if (f(a)) => this
     case _ => None
   }
@@ -40,7 +25,7 @@ case class Some[+A](get: A) extends Option[A] {
   def filterViaMap(f: A => Boolean): Option[A] =
     flatMap(a => if (f(a)) Some(a) else None)
 
-  override def map[B](f: A => B): Option[B] = this match {
+  def map[B](f: A => B): Option[B] = this match {
     case Some(x) => Some(f(x))
     case _ => None
   }
@@ -50,17 +35,17 @@ case class Some[+A](get: A) extends Option[A] {
     else Some(xs.sum/xs.size)
 
 
-  override def variance(xs: Seq[Double]): Option[Double] =
+  def variance(xs: Seq[Double]): Option[Double] =
     mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
 
-  override def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+  def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
 
-//  override def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
-//    case (None, _) => None
-//    case (_, None) => None
-//    case (aOpt, bOpt) => Some(f(aOpt, bOpt))
-//  }
-  override def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+  //  override def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+  //    case (None, _) => None
+  //    case (_, None) => None
+  //    case (aOpt, bOpt) => Some(f(aOpt, bOpt))
+  //  }
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
     a.flatMap(aa => b.map(bb => f(aa, bb)))
 
   def insuranceRateQuote(age: Int, numberOfSpeedingTickets: Int): Double = ???
@@ -72,14 +57,14 @@ case class Some[+A](get: A) extends Option[A] {
     map2(optAge, optTickets)(insuranceRateQuote)
   }
 
-  override def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
     case Cons(h, t) => h.flatMap( hh => sequence(t).map( x => Cons(hh, x)))
   }
 
   def sequenceUsingTraverse[A](a: List[Option[A]]): Option[List[A]] =
     traverse(a)(x => x)
 
-  override def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
     case Nil => Some(Nil)
     case Cons(h, t) => map2(f(h), traverse(t)(f))((a, b) => Cons(a, b))
 
@@ -88,27 +73,11 @@ case class Some[+A](get: A) extends Option[A] {
 
 
 
-case object None extends Option[Nothing] {
-  override def flatMap[B](f: Nothing => Option[B]): Option[B] = None
+case class Some[+A](get: A) extends Option[A]
 
-  override def getOrElse[B >: Nothing](default: => B): B = None[B]
 
-  override def orElse[B >: Nothing](ob: => Option[B]): Option[B] = None
 
-  override def filter(f: Nothing => Boolean): Option[Nothing] = None
-
-  override def map[B](f: Nothing => B): Option[B] = None
-
-  override def variance(xs: Seq[Double]): Option[Double] = None
-
-  override def lift[A, B](f: A => B): Option[A] => Option[B] = None[B]
-
-  override def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = None
-
-  override def sequence[A](a: List[Option[A]]): Option[List[A]] = None
-
-  override def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = None
-}
+case object None extends Option[Nothing]
 
 object OptionRunner extends App {
 
